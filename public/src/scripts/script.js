acb0a2339d85cf349cb236e67a5e875698653285
the-parvez-16 <parvez.k.m.16@gmail.com>
2026-02-02 19:34:52 +0530

    ui: add container for profile and repositories with dummy data

--- public/src/scripts/script.js
+++ public/src/scripts/script.js
@@ -10,12 +10,33 @@ async function consumeData(){
         let data = await getData();
         let repos = await data.json();
 
-        console.log(repos['0'].owner);
+        let profile = repos[0].owner;
+        let pfp = profile.avatar_url;
+        let github_url = profile.html_url;
 
+        let followers = await getFollowers(profile.follower);
         console.log(repos);
+        repos.forEach(repo => {
+            let repo_name = repo.name;
+            let repo_url = repo.html_url;
+            let repo_description = repo.description;
+
+            console.log(repo_name);
+            console.log(repo_url);
+            console.log(repo_description);
+            console.log();
+        });
     } catch (err) {
         console.debug(err);
     }
+}
+
+function fillProfileDetails(){
+
+}
+
+function fillRepositories(){
+    
 }
 
 document.querySelector("#search").addEventListener("click",()=>{
